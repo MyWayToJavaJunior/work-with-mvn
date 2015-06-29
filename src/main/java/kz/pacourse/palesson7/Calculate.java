f5ee220240bb4525f2ba612998808436ed51ce2a
MyWayToJavaJunior <hyperjava@mail.ru>
2015-06-29 14:34:50 +0600

    add Calculate class

---
+++ src/main/java/kz/pacourse/palesson7/Calculate.java
@@ -0,0 +1,16 @@
+package kz.pacourse.palesson7;
+
+/**
+ * Выполняется суммиование входящих, указанных при запуске программе переменных, вывод в консоль.
+ * @author DK
+ * @version 1.0.0
+ */
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println("Calculated .....");
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int summ = first + second;
+        System.out.println("Summ of " + first + " and " + second + " is " + summ);
+    }
+}
