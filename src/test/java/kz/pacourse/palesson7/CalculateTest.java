efb9e1582e8252d937005a297ccbfbdfdc4718c4
MyWayToJavaJunior <hyperjava@mail.ru>
2015-07-02 17:47:22 +0600

    Update: minor changes

--- src/test/java/kz/pacourse/palesson7/CalculateTest.java
+++ src/test/java/kz/pacourse/palesson7/CalculateTest.java
@@ -12,21 +12,21 @@ public class CalculateTest {
     @Test(expected = UserException.class)
     public void notTwoArgsException() throws UserException {
         final String INPUT_ARGS_IS_NOT_TWO[] = {"1"};
-        Calculate calculate = new Calculate();
+        //Calculate calculate = new Calculate();
         Calculate.main(INPUT_ARGS_IS_NOT_TWO);
     }
 
     @Test(expected = UserException.class)
     public void testArgsEmptyThrowsException() throws UserException {
         final String INPUT_ARGS_IS_EMPTY[] = {};
-        Calculate calculate = new Calculate();
+        //Calculate calculate = new Calculate();
         Calculate.main(INPUT_ARGS_IS_EMPTY);
     }
 
     @Test(expected = UserException.class)
     public void testArgsContainZeroThrowsException() throws UserException {
         final String INPUT_ARGS_WITH_ZERO[] = {"1","0"};
-        Calculate calculate = new Calculate();
+        //Calculate calculate = new Calculate();
         Calculate.main(INPUT_ARGS_WITH_ZERO);
     }
 }
